fab97c79dab39ab423ee584533f5b6e0aeeff4e3
RAJASAAB <rajasaab4345@gmail.com>
2026-07-05 18:43:50 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,13 @@
+import socket
+
+host = input("Host: localhost 1")
+for port in range(1, 101):5901
+    s = socket.socket()
+    s.settimeout(0.5)
+    if s.connect_ex((host, port)) == 0:
+        print(f"Port {port} Open")
+    s.close()
+
 import random
 import string
 
